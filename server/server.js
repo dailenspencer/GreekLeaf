d1d98109646f6cf978e55f0a29d7fa04e0a8e7ca
Dailen Spencer <dailenspencer@Dailens-MacBook-Pro.local>
2016-08-15 10:53:40 -0700

    [Heroku] Fix deployment

--- server/server.js
+++ server/server.js
@@ -29,17 +29,10 @@ var server = app.listen(process.env.PORT || 8084, function() {
 
 })
 
-app.get('*', function(req,res){
-  res.sendFile(path.resolve('client', 'index.html'));
-})
-
 app.get('/*', function(req,res){
   res.sendFile(path.resolve('client', 'index.html'));
 })
 
-app.get('/', function(req,res){
-  res.sendFile(path.resolve('client', 'index.html'));
-})
 
 app.get('/Home', function(req,res){
 	res.sendFile(path.resolve('../client/index.html'));
